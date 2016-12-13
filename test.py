a463280822079e156a0f7e15096d335c071ceb5d
David Shaked <ds2643@columbia.edu>
2016-12-12 19:26:56 -0500

    test template added

---
+++ test.py
@@ -0,0 +1,71 @@
+# automatically generated pytest template with ttpy 
+def test___init__():
+    """testing: test___init__"""
+    # TODO: write test...
+    assert (false)
+
+def test_mask():
+    """testing: test_mask"""
+    # TODO: write test...
+    assert (false)
+
+def test_apPixelRaw():
+    """testing: test_apPixelRaw"""
+    # TODO: write test...
+    assert (false)
+
+def test_dabPixelRaw():
+    """testing: test_dabPixelRaw"""
+    # TODO: write test...
+    assert (false)
+
+def test_dab():
+    """testing: test_dab"""
+    # TODO: write test...
+    assert (false)
+
+def test_ap():
+    """testing: test_ap"""
+    # TODO: write test...
+    assert (false)
+
+def test_background():
+    """testing: test_background"""
+    # TODO: write test...
+    assert (false)
+
+def test___init__():
+    """testing: test___init__"""
+    # TODO: write test...
+    assert (false)
+
+def test_contourData():
+    """testing: test_contourData"""
+    # TODO: write test...
+    assert (false)
+
+def test_geoCenters():
+    """testing: test_geoCenters"""
+    # TODO: write test...
+    assert (false)
+
+def test_areaNoise():
+    """testing: test_areaNoise"""
+    # TODO: write test...
+    assert (false)
+
+def test_radii():
+    """testing: test_radii"""
+    # TODO: write test...
+    assert (false)
+
+def test_distanceInClass():
+    """testing: test_distanceInClass"""
+    # TODO: write test...
+    assert (false)
+
+def test_colocalization():
+    """testing: test_colocalization"""
+    # TODO: write test...
+    assert (false)
+
